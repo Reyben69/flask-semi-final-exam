b0a12d430071784da92d2b7dc3b1291620b766c4
Reyben69 <pajuta.reybenedic@cec.edu.ph>
2025-05-06 18:39:20 +0800

    new commit2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'REY BENEDIC PAJUTA<br>BSIT-A<br>SYSTEM INTEGRATION<br>SEMI-FINAL EXAM!'
+    return 'REY BENEDIC PAJUTA<br>BSIT-A<br>SYSTEM INTEGRATION AND ARCHITECTURE<br>SEMI-FINAL EXAM!'
 
 if __name__ == '__main__':
     app.run(debug=True)
